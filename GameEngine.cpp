84b7f45fcb89a95d11ba3c6b072935c1f59ab81a
cnet-sudo <cnetcnetcnet3@gmail.com>
2023-05-25 12:34:31 +0300

    250523_1234

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,48 +1,47 @@
 #include "GameEngine.h"
 
-void GameEngine::input()
-{
+GameEngine::GameEngine() {
+
+	m_window.create(sf::VideoMode(1280, 720), L"Моя игра", sf::Style::Default);
+	m_background.setFillColor(sf::Color::Green);
+	if (!m_icon.loadFromFile("game.png")) exit(3); 
+	m_window.setIcon(128, 128, m_icon.getPixelsPtr());
+
+}
+
+
+
+
+void GameEngine::input() {
+	
 	sf::Event event;
 
-	while (window->pollEvent(event))  
-	{
-		// Обработка события нажатия на клавишу Esc
-		if ((event.key.code == sf::Keyboard::Escape) || (event.type == sf::Event::Closed)) 	window->close(); 
+	while (m_window.pollEvent(event)) {
+
+		if ((event.key.code == sf::Keyboard::Escape) || (event.type == sf::Event::Closed)) 	m_window.close(); 
 	}
 }
 
-void GameEngine::update(sf::Time const& deltaTime)
-{
+void GameEngine::update(sf::Time const& deltaTime) {
+
 
 }
 
-void GameEngine::draw()
-{
-	// Очистка графического окна
-	window->clear();
-	// Отрисовка фона в графическом окне
-	window->draw(background);
-	// Вывод объектов в графическом окне
-	window->display();
+void GameEngine::draw() {
+	
+	m_window.clear();
+	m_window.draw(m_background);
+	m_window.display();
 }
 
-GameEngine::GameEngine()
-{
-	// Получение ссылки на текстуру для прямоугольника 
-	background.setFillColor(sf::Color::Green);
-	if (!icon.loadFromFile("game.png")) exit(3); 
-	window->setIcon(128, 128, icon.getPixelsPtr());
 
-}
 
-void GameEngine::run()
-{
-	// Объявление переменной часы
+void GameEngine::run() {
+
 	sf::Clock clock;
-	// Цикл работает пока окно открыто
-	while (window->isOpen())
-	{
-		// Текущее время присваиваем переменной времени dt
+	
+	while (m_window.isOpen()) {
+		
 		sf::Time dt = clock.restart();
 
 		input();
--- GameEngine.h
+++ GameEngine.h
@@ -1,26 +1,24 @@
 #pragma once
-#include<iostream>
 #include"AssetManager.h"
 
-class GameEngine
-{
+class GameEngine {
+
 public:
-	// Конструктор
+	
 	GameEngine();
-	// Метод запуска игрового цикла
+	
 	void run();
+
 private:
-	AssetManager manager; // Менеджер ресурсов
-	// Умный указатель на графическое окно 
-	std::unique_ptr<sf::RenderWindow> window = std::make_unique<sf::RenderWindow>(sf::VideoMode(1280, 720),L"Моя игра", sf::Style::Default);
-	// Игровой фон в виде прямоугольника
-	sf::RectangleShape background = sf::RectangleShape(sf::Vector2f(1280, 720));
-	sf::Image icon;
-	// Метод обработки событий 
+	
+	AssetManager m_manager; 
+	sf::RenderWindow m_window;
+	sf::RectangleShape m_background = sf::RectangleShape(sf::Vector2f(1280, 720));
+	sf::Image m_icon;
+
 	void input();
-	// Метод обновления переменных, свойств и методов 
 	void update(sf::Time const& deltaTime);
-	// Метод отрисовки объектов в графическом окне
 	void draw();
+
 };
 
